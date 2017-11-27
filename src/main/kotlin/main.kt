310660b7199cd3169dc7170a1aaf6554503999d9
Lukas Forst <lukas.forst@blindspot.ai>
2017-11-27 17:06:20 +0100

    initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    print("hello")
+}
+
+class Hello {
+    fun run() {
+        println("hello world")
+    }
+}
---
+++ src/test/kotlin/HelloTest.kt
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test
+
+internal class HelloTest {
+    @Test
+    fun run() {
+        val a = Hello()
+        a.run()
+    }
+
+}
