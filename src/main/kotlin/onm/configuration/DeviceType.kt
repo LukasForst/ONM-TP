8593bb41c89f1ad8f79a5009e3639351d39f8336
Martin Řepa <repa.martiin@gmail.com>
2017-12-19 12:27:12 +0100

    initial radio

--- src/main/kotlin/onm/configuration/DeviceType.kt
+++ src/main/kotlin/onm/configuration/DeviceType.kt
@@ -28,6 +28,7 @@ enum class DeviceType(
     FRIDGE(1, 2, 0, 0.1),
     OVEN(1, 2, 0, 0.1),
     DRYER(1, 2, 0, 0.1),
-    TELEVISION(1, 3, 0, 0.1);
+    TELEVISION(1, 3, 0, 0.1),
+    RADIO(1, 2, 0, 0.3);
     //todo add device type values and determine power consumption
 }
---
+++ src/main/kotlin/onm/house/devices/Radio.kt
@@ -0,0 +1,60 @@
+package onm.house.devices
+
+import onm.configuration.DeviceType
+import onm.configuration.json.DeviceConfig
+import onm.events.DeviceStartsEvent
+import onm.events.DeviceTurnedOffEvent
+import onm.events.IEventHandler
+import onm.house.places.Room
+import onm.human.HumanControlUnit
+import java.util.*
+
+class Radio(override val id: UUID,
+            eventHandler: IEventHandler,
+            deviceConfig: DeviceConfig,
+            room: Room) : AbstractDevice(DeviceType.RADIO, deviceConfig, eventHandler, room) {
+
+    init {
+        HumanControlUnit.instance.registerDevice(this)
+    }
+
+    private val radioStartsEvent = DeviceStartsEvent(eventHandler, id, "Radio $deviceDescription is turned on.")
+    private val radioTurnedOffEvent = DeviceTurnedOffEvent(eventHandler, id, "Radio $deviceDescription is turned off.")
+
+
+    private fun didItBreak(): Boolean {
+        val brokenEvent = verifyNotBroken(currentErrorProbability)
+        return if (brokenEvent != null) {
+            deviceStateMachine.brokenSate()
+            brokenEvent.raiseEvent()
+            true
+        } else
+            false
+    }
+
+    fun turnOn() {
+        if (deviceStateMachine.currentState.stateType != StateType.TURNED_OFF) {
+            log.error("Radio $deviceDescription is not turned off. Cannot turn it on.")
+        } else {
+            radioStartsEvent.raiseEvent()
+
+            if (didItBreak()) return
+
+            deviceStateMachine.workingState()
+        }
+    }
+
+    fun turnOff() {
+        if (deviceStateMachine.currentState.stateType != StateType.WORKING) {
+            log.error("Radio $deviceDescription is not turned on. Cannot turn it on.")
+        } else {
+            if (didItBreak()) return
+
+            deviceStateMachine.turnedOffState()
+            radioTurnedOffEvent.raiseEvent()
+        }
+
+
+    }
+
+}
